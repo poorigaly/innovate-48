4a08f1c79be73d9abe9f62a0493a098ad4613f45
poorigaly <poori.krish21@gmail.com>
2020-05-15 12:43:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# innovate-48
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/innovate-48)
---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+
+
+  images = Array.from(Array(15), (x, i) => i);
+
+  masonryImages = [
+    { image: 'https://material-components-web.appspot.com/images/photos/3x2/16.jpg' },
+    { image: 'https://material-components-web.appspot.com/images/photos/2x3/1.jpg' },
+    { image: 'https://material-components-web.appspot.com/images/photos/3x2/1.jpg' },
+    { image: 'https://material-components-web.appspot.com/images/photos/2x3/2.jpg' },
+    { image: 'https://material-components-web.appspot.com/images/photos/2x3/3.jpg' },
+    { image: 'https://material-components-web.appspot.com/images/photos/3x2/2.jpg' },
+    { image: 'https://material-components-web.appspot.com/images/photos/2x3/4.jpg' },
+    { image: 'https://material-components-web.appspot.com/images/photos/3x2/3.jpg' },
+    { image: 'https://material-components-web.appspot.com/images/photos/2x3/5.jpg' },
+    { image: 'https://material-components-web.appspot.com/images/photos/3x2/4.jpg' },
+    { image: 'https://material-components-web.appspot.com/images/photos/2x3/6.jpg' },
+    { image: 'https://material-components-web.appspot.com/images/photos/3x2/5.jpg' },
+    { image: 'https://material-components-web.appspot.com/images/photos/2x3/7.jpg' },
+    { image: 'https://material-components-web.appspot.com/images/photos/3x2/6.jpg' },
+    { image: 'https://material-components-web.appspot.com/images/photos/3x2/7.jpg' },
+  ];
+}
